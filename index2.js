08d678b33bce42c5de93dc3456aa9a04e6538ca9
unknown <306887832@qq.com>
2019-02-11 14:31:37 +0800

    新增样式文件

--- index2.js
+++ index2.js
@@ -1,12 +1,10 @@
 window.onload=function(){ 
     const getItem = localStorage.getItem("temp");
     localStorage.setItem("temp",'1');
-    console.log(getItem)
-    var search = window.location.pathname;;
     if(!!getItem){
-        window.location = search + '?ran = ' + Math.random();
+        window.location = window.location.pathname + '?ran = ' + Math.random();
         localStorage.removeItem("temp");
-        console.log('进入')
+        return false;
     }
-
+    document.getElementById('commodity').style.display = '';
 }

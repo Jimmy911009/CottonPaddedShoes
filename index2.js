ac41d20218d8661b75d20e39bd50027680d65b15
unknown <306887832@qq.com>
2019-02-11 11:47:00 +0800

    增加价格

--- index2.js
+++ index2.js
@@ -1,4 +1,14 @@
 window.onload=function(){ 
+    const getItem = localStorage.getItem("temp");
+    localStorage.setItem("temp",'1');
+    console.log(getItem)
+    var search = window.location.pathname;
+    if(!!getItem){
+        window.location = search + '?ran = ' + Math.random();
+        localStorage.removeItem("temp");
+        console.log('进入')
+    }
     var Price = document.getElementById('Price');
     Price.innerText = '1234'
+
 }

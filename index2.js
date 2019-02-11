a74aa9a0902b4775f24a5b715b0be958f7a3b554
unknown <306887832@qq.com>
2019-02-11 14:08:37 +0800

    修改js

--- index2.js
+++ index2.js
@@ -3,12 +3,12 @@ window.onload=function(){
     localStorage.setItem("temp",'1');
     console.log(getItem)
     var search = window.location.pathname;
+    var Price = document.getElementById('Price');
     if(!!getItem){
         window.location = search + '?ran = ' + Math.random();
         localStorage.removeItem("temp");
         console.log('进入')
+        Price.innerText = '1234'
     }
-    var Price = document.getElementById('Price');
-    Price.innerText = '1234'
 
 }

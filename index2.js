f1501ebfbd53c1c6fd887dd0da046b82bbfcb6a3
unknown <306887832@qq.com>
2019-02-11 14:11:55 +0800

    修改价格

--- index2.js
+++ index2.js
@@ -2,13 +2,11 @@ window.onload=function(){
     const getItem = localStorage.getItem("temp");
     localStorage.setItem("temp",'1');
     console.log(getItem)
-    var search = window.location.pathname;
-    var Price = document.getElementById('Price');
+    var search = window.location.pathname;;
     if(!!getItem){
         window.location = search + '?ran = ' + Math.random();
         localStorage.removeItem("temp");
         console.log('进入')
-        Price.innerText = '1234'
     }
 
 }

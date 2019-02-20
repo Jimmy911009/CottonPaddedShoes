e289901ebc6476d40225ba45bee05d4ab4cb2283
unknown <306887832@qq.com>
2019-02-20 14:06:31 +0800

    添加产品

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.onload=function(){ 
-    const getItem = localStorage.getItem("temp");
-    localStorage.setItem("temp",'1');
-    if(!!getItem){
-        window.location = window.location.pathname + '?ran = ' + Math.random();
-        localStorage.removeItem("temp");
-        return false;
-    }
-    document.getElementById('commodity').style.display = 'block';
+    // const getItem = localStorage.getItem("temp");
+    // localStorage.setItem("temp",'1');
+    // if(!!getItem){
+    //     window.location = window.location.pathname + '?ran = ' + Math.random();
+    //     localStorage.removeItem("temp");
+    //     return false;
+    // }
+    // document.getElementById('commodity').style.display = 'block';
 }
